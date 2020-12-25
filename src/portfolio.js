27f9c5ba7da5aea581a88e452afebf750ac8ba86
Aditya Saxena <adityasaxenamail@gmail.com>
2020-12-25 18:31:37 +0530

    my portfolio website

---
+++ src/portfolio.js
@@ -0,0 +1,588 @@
+/* Change this file to get your personal Porfolio */
+
+// Website related settings
+const settings = {
+  isSplash: false, // Change this to false if you don't want Splash screen.
+};
+
+//Home Page
+const greeting = {title: "Aditya Saxena",
+logo_name: "AdityaSaxena",
+nickname: "Tech Enthusiast",
+subTitle:
+  "A passionate individual who always thrive to work on end to end products which develop sustainable and scalable social and technical systems to create impact.",
+resumeLink:
+  "https://drive.google.com/file/d/1JSq85AXiN4a4Tw5zqvU0ds3XestBby0a/view?usp=sharing",
+portfolio_repository: "https://github.com/adityasaxenatech",
+};
+
+const socialMediaLinks = [
+  /* Your Social Media Link */
+  // github: "https://github.com/ashutosh1919",
+  // linkedin: "https://www.linkedin.com/in/ashutosh-hathidara-88710b138/",
+  // gmail: "ashutoshhathidara98@gmail.com",
+  // gitlab: "https://gitlab.com/ashutoshhathidara98",
+  // facebook: "https://www.facebook.com/laymanbrother.19/",
+  // twitter: "https://twitter.com/ashutosh_1919",
+  // instagram: "https://www.instagram.com/layman_brother/"
+
+  {
+    name: "Github",
+    link: "https://github.com/adityasaxenatech",
+    fontAwesomeIcon: "fa-github", // Reference https://fontawesome.com/icons/github?style=brands
+    backgroundColor: "#181717", // Reference https://simpleicons.org/?q=github
+  },
+  {
+    name: "LinkedIn",
+    link: "https://www.linkedin.com/in/iamadityasaxena/",
+    fontAwesomeIcon: "fa-linkedin-in", // Reference https://fontawesome.com/icons/linkedin-in?style=brands
+    backgroundColor: "#0077B5", // Reference https://simpleicons.org/?q=linkedin
+  },
+  {
+    name: "YouTube",
+    link: "https://www.youtube.com/c/AdityaSaxenaMusic/",
+    fontAwesomeIcon: "fa-youtube", // Reference https://fontawesome.com/icons/youtube?style=brands
+    backgroundColor: "#FF0000", // Reference https://simpleicons.org/?q=youtube
+  },
+  {
+    name: "Gmail",
+    link: "mailto:adityasaxenamail@gmail.com",
+    fontAwesomeIcon: "fa-google", // Reference https://fontawesome.com/icons/google?style=brands
+    backgroundColor: "#D14836", // Reference https://simpleicons.org/?q=gmail
+  },
+  {
+    name: "Twitter",
+    link: "https://twitter.com/iadityasaxena",
+    fontAwesomeIcon: "fa-twitter", // Reference https://fontawesome.com/icons/twitter?style=brands
+    backgroundColor: "#1DA1F2", // Reference https://simpleicons.org/?q=twitter
+  },
+  {
+    name: "Facebook",
+    link: "https://www.facebook.com/aditya.saxena.14661/",
+    fontAwesomeIcon: "fa-facebook-f", // Reference https://fontawesome.com/icons/facebook-f?style=brands
+    backgroundColor: "#1877F2", // Reference https://simpleicons.org/?q=facebook
+  },
+  {
+    name: "Instagram",
+    link: "https://www.instagram.com/adityasaxenamusic/",
+    fontAwesomeIcon: "fa-instagram", // Reference https://fontawesome.com/icons/instagram?style=brands
+    backgroundColor: "#E4405F", // Reference https://simpleicons.org/?q=instagram
+  },
+];
+
+const skills =  {
+  data: [
+    {
+      title: "Digital Marketing & BI",
+      fileName: "DataScienceImg",
+      skills: [
+        "⚡ Developing highly scalable production ready models for various Digital marketing and Business intelligence use cases",
+        "⚡ Experience of working with Digital marketing projects used for Data analysis",
+        "⚡ Complex quantitative modelling for SEO, SEM, Content automation & marketing, Data-driven marketing analysis and Socialmedia marketing"
+      ],
+      softwareSkills: [
+        {
+          skillName: "",
+          fontAwesomeClassname: "",
+          style: {
+            backgroundColor: "transparent"
+          }
+        },
+        {
+          skillName: "",
+          fontAwesomeClassname: "",
+          style: {
+            backgroundColor: "white",
+            color: "#D00000"
+          }
+        },
+        {
+          skillName: "",
+          fontAwesomeClassname: "",
+          style: {
+            backgroundColor: "transparent"
+          }
+        },
+        {
+          skillName: "",
+          fontAwesomeClassname: "",
+          style: {
+            backgroundColor: "transparent",
+            color: "#3776AB"
+          }
+        }
+      ]
+    },
+    {
+      title: "Web Development",
+      fileName: "FullStackImg",
+      skills: [
+        "⚡ Building resposive website front end using HTML, JS, CSS",
+        "⚡ Developing mobile applications",
+        "⚡ Creating website in Wordpress & Blogger "
+      ],
+      softwareSkills: [
+        {
+          skillName: "",
+          fontAwesomeClassname: "",
+          style: {
+            color: "#E34F26"
+          }
+        },
+        {
+          skillName: "",
+          fontAwesomeClassname: "",
+          style: {
+            color: "#1572B6"
+          }
+        },
+        {
+          skillName: "",
+          fontAwesomeClassname: "",
+          style: {
+            color: "#CC6699"
+          }
+        },
+        {
+          skillName: "",
+          fontAwesomeClassname: "",
+          style: {
+            backgroundColor: "#000000",
+            color: "#F7DF1E"
+          }
+        },
+        {
+          skillName: "",
+          fontAwesomeClassname: "",
+          style: {
+            color: "#61DAFB"
+          }
+        },
+        {
+          skillName: "",
+          fontAwesomeClassname: "",
+          style: {
+            color: "#339933"
+          }
+        },
+        {
+          skillName: "",
+          fontAwesomeClassname: "",
+          style: {
+            color: "#CB3837"
+          }
+        },
+        {
+          skillName: "",
+          fontAwesomeClassname: "",
+          style: {
+            color: "#2C8EBB"
+          }
+        },
+        {
+          skillName: "",
+          fontAwesomeClassname: "",
+          style: {
+            color: "#663399"
+          }
+        },
+        {
+          skillName: "",
+          fontAwesomeClassname: "",
+          style: {
+            color: "#02569B"
+          }
+        }
+      ]
+    },
+    {
+      title: "Cloud Infra-Architecture And IoT",
+      fileName: "CloudInfraImg",
+      skills: [
+        "⚡ Experience working on multiple cloud platforms",
+        "⚡ Hosting and maintaining websites on virtual machine instances along with integration of databases",
+        "⚡ Deploying Internet of things models for embedded system to use for devlopment of new techprojects",
+        
+      ],
+      softwareSkills: [
+        {
+          skillName: "",
+          fontAwesomeClassname: "",
+          style: {
+            color: "#4285F4"
+          }
+        },
+        {
+          skillName: "",
+          fontAwesomeClassname: "",
+          style: {
+            color: "#FF9900"
+          }
+        },
+        {
+          skillName: "",
+          fontAwesomeClassname: "",
+          style: {
+            color: "#0089D6"
+          }
+        },
+        {
+          skillName: "",
+          fontAwesomeClassname: "",
+          style: {
+            color: "#FFCA28"
+          }
+        },
+        {
+          skillName: "",
+          fontAwesomeClassname: "",
+          style: {
+            color: "#336791"
+          }
+        },
+        {
+          skillName: "",
+          fontAwesomeClassname: "",
+          style: {
+            color: "#47A248"
+          }
+        },
+        {
+          skillName: "",
+          fontAwesomeClassname: "",
+          style: {
+            color: "#1488C6"
+          }
+        },
+        {
+          skillName: "",
+          fontAwesomeClassname: "",
+          style: {
+            color: "#326CE5"
+          }
+        }
+      ]
+    },
+    {
+      title: "UI/UX Design",
+      fileName: "DesignImg",
+      skills: [
+        "⚡ Designing highly attractive user interface for mobile and web applications",
+        "⚡ Customizing logo designs and building logos from scratch",
+        "⚡ Creating the flow of application functionalities to optimize user experience"
+      ],
+      softwareSkills: [
+        {
+          skillName: "",
+          fontAwesomeClassname: "",
+          style: {
+            color: "#FF2BC2"
+          }
+        },
+        {
+          skillName: "Figma",
+          fontAwesomeClassname: "",
+          style: {
+            color: "#F24E1E"
+          }
+        },
+        {
+          skillName: "",
+          fontAwesomeClassname: "",
+          style: {
+            color: "#FF7C00"
+          }
+        },
+        {
+          skillName: "",
+          fontAwesomeClassname: "",
+          style: {
+            color: "#000000"
+          }
+        }
+      ]
+    }
+  ]
+};
+
+
+// Education Page
+const competitiveSites = {
+  competitiveSites: [
+    {
+      siteName: "",
+      iconifyClassname: "",
+      style: {
+        color: "#2EC866"
+      },
+      profileLink: ""
+    },
+    {
+      siteName: "",
+      iconifyClassname: "",
+      style: {
+        color: "#5B4638"
+      },
+      profileLink: ""
+    },
+    {
+      siteName: "",
+      iconifyClassname: "",
+      style: {
+        color: "#1F8ACB"
+      },
+      profileLink: ""
+    },
+    {
+      siteName: "",
+      iconifyClassname: "",
+      style: {
+        color: "#323754"
+      },
+      profileLink: ""
+    },
+    {
+      siteName: "",
+      iconifyClassname: "",
+      style: {
+        color: "#20BEFF"
+      },
+      profileLink: ""
+    }
+  ]
+}
+
+const degrees = {
+  degrees : [
+    {
+      title: "Rajasthan Technical University,Kota (SKIT)",
+      subtitle: "B.Tech. in Electronics And Communication Engineering",
+      logo_path: "iiitk_logo.png",
+      alt_name: "IIITDM Kurnool",
+      duration: "2015 - 2019",
+      descriptions: [
+        "⚡ I have studied basic engineering subjects like DS, Algorithms, Electronics, Communication Networks, Micro Processors, Iot etc.",
+        "⚡ Apart from this, I have done courses on Web Development, Networking, Digital Marketing, Cloud Computing and Cyber Security.",
+		"⚡ My Project was selected for The Finals Of IEEE YESIST 12 Held at Stanford University,Thailand.",
+		"⚡ Awarded With Honours Distinctions for Instrumetal Music Recital across Interstate Cultural Programs.",
+
+      ],
+      website_link: "http://www.rtu.ac.in/home/"
+    }
+  ]
+}
+
+const certifications = {
+	certifications: [
+		{
+			title: "Letter of Recognition",
+			subtitle: "- HoD,ECE",
+			logo_path: "stanford_logo.png",
+			certificate_link:
+				"https://drive.google.com/file/d/1VFNVSJUTuSOXHp_nexhBRhIJxKEbAn6M/view?usp=sharing",
+			alt_name: "Stanford University",
+			color_code: "#8C151599"
+		},
+		{
+			title: "Certificate of completion ",
+			subtitle: "- Deloitte",
+			logo_path: "deeplearning_ai_logo.png",
+			certificate_link:
+				"https://www.linkedin.com/posts/iamadityasaxena_virtualinternship-deloitteconsulting-communication-activity-6687749667050151936-9Yzz",
+			alt_name: "deeplearning.ai",
+			color_code: "#00000099"
+		},
+		{
+			title: "Google Ads Fundamentals",
+			subtitle: "Credential ID-14869056",
+			logo_path: "google_logo.png",
+			certificate_link:	"",
+			alt_name: "Google",
+			color_code: "#0C9D5899"
+		},
+		{
+			title: "Google Analytics Individual Qualication",
+      subtitle: "Google Analytics for Business                 Credential ID-14872551        ",
+                logo_path: "google_logo.png",
+			certificate_link:"",
+			alt_name: "Google",
+			color_code: "#0C9D5899"
+    },
+    {
+			title: "Cloud Architecture",
+			subtitle: "- Qwiklabs",
+			logo_path: "gcp_logo.png",
+			certificate_link:	"",
+			alt_name: "GCP",
+			color_code: "#4285F499"
+		}
+	]
+};
+
+
+// Experience Page
+const experience = {
+	title: "Experience",
+	subtitle: "Work, Internship and Volunteership",
+	description:
+		" My hard work, curiosity, and enthusiasm to learn something new every day have worked as a perfect package for me to handle multiple domains including Software Development, Digital Marketing, Project Management, and Public Relations. The Technical Industry fascinates me and I'd love to work on research areas,industry grade projects focusing on vital issues and latest technologies.I love organising events and that is why I am also involved with many Volunteer communities as a representative.",
+	header_image_path: "experience.svg",
+	sections: [
+		{
+			title: "Work",
+			experiences: [
+				{
+					title: "Engineer Trainee",
+					company: "Bosch",
+					company_url: "https://in.linkedin.com/company/bosch-engineering-and-business-solutions?trk=affiliated-pages_result-card_full-click",
+					logo_path: "muffito_logo.png",
+					duration: "Sept 2020 - Present",
+					location: "Jaipur, Rajasthan",
+					description:
+						"I am currently working under technical engineering function department of bosch jaipur. I am involved in a team for providing complete instumentation architecure solution and innovation application as engineer trainee for company.",
+					color: "#9b1578",
+				},
+			],
+		},
+		{
+			title: "Internships",
+			experiences: [
+				{
+					title: "Technology Consulting Virtual Intern",
+					company: "Deloitte",
+					company_url: "https://www.linkedin.com/company/deloitte-india/",
+					logo_path: "de.png",
+					duration: "May 2020 – Sept 2020",
+					location: "",
+					description:
+						"I have worked on project of provideing technology cunsulting. There were three takeaways:(1) Technology Strategy & Innovation- What technology aspects should be considered before developing an online banking platform,Develop a high-level implementation plan and predict cost estimates.(2) Optimization & Delivery- Conduct an initial market scan to evaluate and select a new financial accounting system,  Conduct further analysis on the selected vendors, summarize your findings and proposed solution.(3) Cloud Engineering- Prepare a high-level overview of Cloud Computing for a client meeting, Conduct an analysis and recommend which applications are suitable for transitioning to Cloud.",
+					color: "#ee3c26",
+				},
+				{
+					title: "Intern",
+					company: "Corporate Infocom Pvt.Ltd.",
+					company_url:
+						"https://www.linkedin.com/company/corporate-infocom-pvt-ltd-/",
+					logo_path: "cipl.jpg",
+					duration: "Mar 2016 - Apr 2018",
+					location: "Jaipur",
+					description:
+						"Developed a website for different customers to help them grow in online market. and Conceptualized and designed a Digital Campaign for the Corporate Infocom using Google Ad words. Worked with the Sales team of corporate infocom and had over 1000 registrations in a month.",
+					color: "#0071C5",
+        },
+        {title: "Summer Intern",
+        company: "Indian Railways-North Western Region,.",
+        company_url:
+          "https://nwr.indianrailways.gov.in/",
+        logo_path: "rail.png",
+        duration: "May 2018 - Jul 2018",
+        location: "Jaipur",
+        description:
+          "Learnt: About various Division Under Electronics and Communication Engineering Domains.",
+        color: "#0071C5",
+      },
+			],
+		},
+		{
+			title: "Volunteerships",
+			experiences: [
+				{
+					title: "Social Media Marketing Volunteer",
+					company: "SPIC MACAY",
+					company_url: "https://spicmacay.org/",
+					logo_path: "SPIC.png",
+					duration: "June 2016 - Present",
+					location: "Jaipur, Rajasthan",
+					description:
+						"Reported weekly status and coordinated handshake calls with the onsite team Organized workplace celebrations to contribute to teambuilding activities as Team Cultural Meet up Coordinated the event SPIC MACAY MUSIC IN THE PARK.",
+					color: "#4285F4",
+				},
+				{
+					title: "Campus Representative",
+					company: "E-CELL IIT BOMBAY",
+					company_url: "https://www.ecell.in/2019/",
+					logo_path: "E-Cel.png",
+					duration: "Aug 2017 - May 2018",
+					location: "Jaipur, Rajasthan",
+					description:
+						"The College Representative program involved coordinating with The Entrepreneurship Cell team to reach out to all the students in the representatives’ respective college for publicising E-Cell's initiatives, promoting E-Cell's competitions and orchestrating events in their institute with support from E-Cell, IIT Bombay",
+					color: "#D83B01",
+				},
+				{
+					title: "Mozilla Campus Captain",
+					company: "Mozilla",
+					company_url: "https://www.mozilla.org/",
+					logo_path: "mozilla_logo.png",
+					duration: "Oct 2018 - May 2019",
+					location: "Jaipur, Rajasthan",
+					description:
+						"My responsibility for this program was to create opensource environment in college and in the city. We have organised multiple programs on the problems collected by ordinary people from Jaipur city.",
+					color: "#000000",
+				},
+				{
+					title: "Skit Embedded System Students Club Member",
+					company: "SKIT",
+					company_url:
+						"",
+					logo_path: "skit.png",
+					duration: "Jan 2017 - May 2019",
+					location: "Jaipur, Rajasthan",
+					description:
+						"We have well established embedded club in college which is directly associated with devlopment of new tech projects for various competition and hackathons. We have developed many interdisciplinary projects under the membership of this club. We have organised workshops and activities on embedded and iot projects.",
+					color: "#0C9D58",
+				},
+			],
+		},
+	],
+};
+
+// Projects Page
+const projectsHeader = {
+	title: "Projects",
+	description:
+		"My projects makes use of vast variety of latest technology tools. My best experience is to create projects and deploy them to web applications using cloud infrastructure.",
+	avatar_image_path: "projects_image.svg",
+};
+
+
+// Contact Page
+const contactPageData = {
+	contactSection: {
+		title: "Contact Me",
+		profile_image_path: "animated_ashutosh.png",
+		description:
+			"I am available on almost every social media. You can message me, I will reply within 24 hours.",
+	},
+	blogSection: {
+		title: "My Youtube Videos ",
+		subtitle:
+			"For individual fundamental development, I like to play instrumental music that create impact on each of the listner individually to listen the core of indian music.",
+		link: "https://www.youtube.com/c/AdityaSaxenaMusic",
+		avatar_image_path: "blogs_image.svg",
+	},
+	addressSection: {
+		title: "Address",
+		subtitle:
+			"Elegance Apartments, Sodala, N.S Road, Dist.-Jaipur, Rajasthan - 302019",
+		avatar_image_path: "address_image.svg",
+		location_map_link: "https://goo.gl/maps/dDcKZ4ZYPthjRHf26",
+	},
+	phoneSection: {
+		title: "Phone Number",
+		subtitle: "+91 8619243464",
+	},
+};
+
+export {
+	settings,
+	greeting,
+	socialMediaLinks,
+	skills,
+	competitiveSites,
+	degrees,
+	certifications,
+	experience,
+	projectsHeader,
+	contactPageData,
+};
+
