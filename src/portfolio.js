c45d1c20d3164751e3f193535022befb02b9492e
Aditya Saxena <adityasaxenamail@gmail.com>
2021-01-03 18:20:53 +0530

    changed

--- src/portfolio.js
+++ src/portfolio.js
@@ -17,14 +17,6 @@ portfolio_repository: "https://github.com/adityasaxenatech",
 };
 
 const socialMediaLinks = [
-  /* Your Social Media Link */
-  // github: "https://github.com/ashutosh1919",
-  // linkedin: "https://www.linkedin.com/in/ashutosh-hathidara-88710b138/",
-  // gmail: "ashutoshhathidara98@gmail.com",
-  // gitlab: "https://gitlab.com/ashutoshhathidara98",
-  // facebook: "https://www.facebook.com/laymanbrother.19/",
-  // twitter: "https://twitter.com/ashutosh_1919",
-  // instagram: "https://www.instagram.com/layman_brother/"
 
   {
     name: "Github",
